34087b7c78b1c684cfed70fa0fbdb689e978e662
Noah Durbin <13364668+noahdurbin@users.noreply.github.com>
2024-07-27 18:00:42 -0600

    feat: edited the method to return json in the response

--- app/services/openai_service.rb
+++ app/services/openai_service.rb
@@ -6,14 +6,16 @@ class OpenaiService
   end
 
   def tell_me_more(topic)
-    prompt = "Tell me more about #{topic}"
+    prompt = "Tell me more about #{topic}. please give me the response in json and only json format. do not include any other information. only a JSON object, do not put it in an array, in quotes or anything else.I want the json object to have the key data with only description nested under data"
     client = OpenAI::Client.new
     response = client.chat(
       parameters: {
         model: "gpt-4o-mini-2024-07-18",
+        response_format: { type: "json_object" },
         messages: [{ role: "user", content: "#{prompt}"}],
         temperature: 0.5,
       })
-    response.dig("choices", 0, "message", "content")
+    content = response.dig("choices", 0, "message", "content")
+    JSON.parse(content, symbolize_names: true)
   end
 end
