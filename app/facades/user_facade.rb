ff0ed11b2b349a5c93ab4aef744f35ca58231030
Noah Durbin <13364668+noahdurbin@users.noreply.github.com>
2024-08-01 10:43:42 -0600

    user show
    added route to get current user details

--- app/facades/user_facade.rb
+++ app/facades/user_facade.rb
@@ -2,7 +2,12 @@ class UserFacade
   def create_user(params)
     UserService.new.create_user(params)
   end
-  
+
+  def get_user(id)
+    user_detail = UserService.new.get_user(id)
+    User.new(user_detail)
+  end
+
   def login(params)
     UserService.new.login(params)
   end
--- app/services/user_service.rb
+++ app/services/user_service.rb
@@ -8,6 +8,13 @@ class UserService
     json[:data]
   end
 
+  def get_user(id)
+    response = Faraday.get("https://coding-on-the-john-be-e2b03178993f.herokuapp.com/api/v1/users/#{id}") do |request|
+    end
+    json = JSON.parse(response.body, symbolize_names: true)
+    json[:data]
+  end
+
   def oauth
     response = Faraday.get("https://coding-on-the-john-be-e2b03178993f.herokuapp.com/api/v1/auth/github") do |request|
     end
