da83c0fa4178a5bde45cbc7aadcc3bebed8265b4
Garrett Bowman <garrettbowman248@gmail.com>
2024-08-01 16:01:12 -0500

    Feat: Fix bug

--- app/facades/github_facade.rb
+++ app/facades/github_facade.rb
@@ -2,6 +2,6 @@ class GithubFacade
   def self.github_user(code)
     access_token = GithubService.fetch_github_token(code)
     github_user = GithubService.fetch_github_user(access_token)
-    GithubService.create_or_signin_github_user(github_user)
+    GithubService.create_or_signin_github_user(github_user, access_token)
   end
 end
