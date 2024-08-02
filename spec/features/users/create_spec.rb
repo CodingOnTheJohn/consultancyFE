9d01c886689c6a09720035cdc2adbe6f4b8e1cc8
Dana Howell <howelld115@gmail.com>
2024-08-01 19:46:16 -0600

    extraneous tests deleted, still vcr error

--- spec/features/users/create_spec.rb
+++ spec/features/users/create_spec.rb
@@ -9,16 +9,7 @@ RSpec.describe 'Create User', type: :feature do
         phone_num = "444-555-6666"
         password = "test"
         password_confirmation = "test"
-        
-        visit root_path
-
-        expect(page).to have_link("Sign Up")
-
-        within(".navbar") do
-          click_on "Sign Up"
-        end
-
-        expect(current_path).to eq(new_user_path)
+        visit new_user_path
         fill_in :username, with: username
         fill_in :email, with: email
         fill_in :phone_number, with: phone_num
@@ -31,197 +22,84 @@ RSpec.describe 'Create User', type: :feature do
         expect(current_path).to eq(root_path)
         expect(page).to have_content("Welcome #{username}")
 
-        expect(page).not_to have_link("Sign Up")
+        expect(page).not_to have_link('Sign Up')
         expect(page).not_to have_link("Log In")
       end
     end 
 
-    #Dana's ========================================
-    # use Webmock for sad paths ?
-    # describe "Sad Paths" do
-    #   describe "username" do 
-    #     before do 
-    #       visit root_path
-    #       expect(current_path).to eq(root_path)
-    #       click_on "Sign Up"
-    #       expect(current_path).to eq(new_user_path)
-    #       @username = "acjtesting5"
-    #       @email = "test4554@example.com"
-    #       @phone_num = "444-555-6666"
-    #       @password = "test"
-    #       @password_confirmation = "test"
-    #     end
+    describe "Sad Paths" do
+      describe "username" do 
+        it "username already exists" do 
+          visit new_user_path
+          # let!(:usberer) { { "id"=>"12", "type"=>"user", "attributes"=>{ "email"=>"test4554@example.com", "username"=>"acjtesting5" } } }
+          @username = "acjtesting5"
+          @email = "test4554@example.com"
+          @phone_num = "444-555-6666"
+          @password = "test"
+          @password_confirmation = "test"
+          
 
-    #     it "User create page: missing params - username" do
-    #       fill_in :username, with: " "
-    #       fill_in :email, with: @email
-    #       fill_in :phone_number, with: @phone_num
-    #       check :text_preference
-    #       fill_in :password, with: @password
-    #       fill_in :password_confirmation, with: @password_confirmation
+          fill_in :username, with: @username
+          fill_in :email, with: @email
+          fill_in :phone_number, with: @phone_num
+          check :text_preference
+          fill_in :password, with: @password
+          fill_in :password_confirmation, with: @password_confirmation
           
-    #       click_on "Create User"
+          click_on "Create User"
           
-    #       expect(current_path).to eq(new_user_path)
-    #       expect(page).to have_content("Username can't be blank")
-    #     end
+          expect(current_path).to eq(new_user_path)
+          expect(page).to have_content("Name can't be blank")
+        end
+      end
 
-    #     it "User create page: at least three characters" do
-    #       fill_in :username, with: "yz"
-    #       fill_in :email, with: @email
-    #       fill_in :phone_number, with: @phone_num
-    #       check :text_preference
-    #       fill_in :password, with: @password
-    #       fill_in :password_confirmation, with: @password_confirmation
-          
-    #       click_on "Create User"
-          
-    #       expect(current_path).to eq(new_user_path)
-    #       expect(page).to have_content("Username is too short (minimum is 3 characters)")
-    #     end
+      describe "email" do
+        it "User create page: invalid email" do
+          visit new_user_path
 
-    #     # it "username already exists" do 
-    #     #   let!(:user) { { "id"=>"12", "type"=>"user", "attributes"=>{ "email"=>"test4554@example.com", "username"=>"acjtesting5" } } }
+          @username = "acjtesting5"
+          @email = "test4554@example.com"
+          @phone_num = "444-555-6666"
+          @password = "test"
+          @password_confirmation = "test"
 
-    #     #   fill_in :username, with: @username
-    #     #   fill_in :email, with: @email
-    #     #   fill_in :phone_number, with: @phone_num
-    #     #   check :text_preference
-    #     #   fill_in :password, with: @password
-    #     #   fill_in :password_confirmation, with: @password_confirmation
+          fill_in :username, with: @username
+          fill_in :email, with: "bademail"
+          fill_in :phone_number, with: @phone_num
+          check :text_preference
+          fill_in :password, with: @password
+          fill_in :password_confirmation, with: @password_confirmation
           
-    #     #   click_on "Create User"
+          click_on "Create User"
           
-    #     #   expect(current_path).to eq(new_user_path)
-    #     #   expect(page).to have_content("Name can't be blank")
-    #     # end
-    #   end
-
-    #   describe "email" do
-    #     before do 
-    #       visit root_path
-    #       expect(current_path).to eq(root_path)
-    #       click_on "Sign Up"
-    #       expect(current_path).to eq(new_user_path)
-    #       @username = "acjtesting5"
-    #       @email = "test4554@example.com"
-    #       @phone_num = "444-555-6666"
-    #       @password = "test"
-    #       @password_confirmation = "test"
-    #     end
-
-    #     it "User create page: invalid email" do
-    #       fill_in :username, with: @username
-    #       fill_in :email, with: "bademail"
-    #       fill_in :phone_number, with: @phone_num
-    #       check :text_preference
-    #       fill_in :password, with: @password
-    #       fill_in :password_confirmation, with: @password_confirmation
-          
-    #       click_on "Create User"
-          
-    #       expect(current_path).to eq(new_user_path)
-    #       expect(page).to have_content("Email is invalid")
-    #     end
-
-    #     it "User create page: missing params - email" do
-    #       fill_in :username, with: @username
-    #       fill_in :email, with: " "
-    #       fill_in :phone_number, with: @phone_num
-    #       check :text_preference
-    #       fill_in :password, with: @password
-    #       fill_in :password_confirmation, with: @password_confirmation
-          
-    #       click_on "Create User"
-          
-    #       expect(current_path).to eq(new_user_path)
-    #       expect(page).to have_content("Email can't be blank")
-    #     end
-    #   end
-
-    #   describe "phone number" do
-    #     before do 
-    #       visit root_path
-    #       expect(current_path).to eq(root_path)
-    #       click_on "Sign Up"
-    #       expect(current_path).to eq(new_user_path)
-    #       @username = "acjtesting5"
-    #       @email = "test4554@example.com"
-    #       @phone_num = "444-555-6666"
-    #       @password = "test"
-    #       @password_confirmation = "test"
-    #     end
-
-    #     it "User create page: missing params - phone_number" do
-    #       fill_in :username, with: @username
-    #       fill_in :email, with: @email
-    #       fill_in :phone_number, with: " "
-    #       check :text_preference
-    #       fill_in :password, with: @password
-    #       fill_in :password_confirmation, with: @password_confirmation
-          
-    #       click_on "Create User"
-          
-    #       expect(current_path).to eq(new_user_path)
-    #       expect(page).to have_content("Phone number can't be blank")
-    #     end
-
-    #     it "User create page: phone_number invalid" do
-    #       fill_in :username, with: @username
-    #       fill_in :email, with: @email
-    #       fill_in :phone_number, with: "invalid-phone-number "
-    #       check :text_preference
-    #       fill_in :password, with: @password
-    #       fill_in :password_confirmation, with: @password_confirmation
-          
-    #       click_on "Create User"
-          
-    #       expect(current_path).to eq(new_user_path)
-    #       expect(page).to have_content("Phone number is invalid")
-    #     end
-    #   end
-      
-    #   describe "password" do
-    #     before do 
-    #       visit root_path
-    #       expect(current_path).to eq(root_path)
-    #       click_on "Sign Up"
-    #       expect(current_path).to eq(new_user_path)
-    #       @username = "acjtesting5"
-    #       @email = "test4554@example.com"
-    #       @phone_num = "444-555-6666"
-    #       @password = "test"
-    #       @password_confirmation = "test"
-    #     end
+          expect(current_path).to eq(new_user_path)
+          expect(page).to have_content("Email is invalid")
+        end
+      end
         
-    #     it "User create page: missing params - password" do
-    #       fill_in :username, with: @username
-    #       fill_in :email, with: @email
-    #       fill_in :phone_number, with: @phone_num
-    #       check :text_preference
-    #       fill_in :password, with: " "
-    #       fill_in :password_confirmation, with: @password_confirmation
+      describe "passwords" do
+        it "User create page: passwords don't match" do
+          visit new_user_path
+
+          @username = "acjtesting5"
+          @email = "test4554@example.com"
+          @phone_num = "444-555-6666"
+          @password = "test"
+          @password_confirmation = "test"
+
+          fill_in :username, with: @username
+          fill_in :email, with: @email
+          fill_in :phone_number, with: @phone_num
+          check :text_preference
+          fill_in :password, with: @password
+          fill_in :password_confirmation, with:"sets"
           
-    #       click_on "Create User"
+          click_on "Create User"
           
-    #       expect(current_path).to eq(new_user_path)
-    #       expect(page).to have_content("Password can't be blank")
-    #     end
-        
-    #     it "User create page: passwords don't match" do
-    #       fill_in :username, with: @username
-    #       fill_in :email, with: @email
-    #       fill_in :phone_number, with: @phone_num
-    #       check :text_preference
-    #       fill_in :password, with: @password
-    #       fill_in :password_confirmation, with:"sets"
-          
-    #       click_on "Create User"
-          
-    #       expect(current_path).to eq(new_user_path)
-    #       expect(page).to have_content("Passwords don't match")
-    #     end
-    #   end
-    # end
+          expect(current_path).to eq(new_user_path)
+          expect(page).to have_content("Passwords don't match")
+        end
+      end
+    end
   end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -69,6 +69,6 @@ RSpec.configure do |config|
   VCR.configure do |config|
     config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
     config.hook_into :webmock
-    # config.configure_rspec_metadata!
+    config.configure_rspec_metadata!
   end
 end
